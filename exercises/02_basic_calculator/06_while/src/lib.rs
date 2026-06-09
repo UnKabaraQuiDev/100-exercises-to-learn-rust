6e9084c771d020988c1cea71620dd7ec4df9801e
UnKabaraQuiDev <u.kbra.lu@gmail.com>
2026-06-09 19:57:53 +0200

    done up to 03_ticket_v1/03_modules

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -1,10 +1,12 @@
 // Rewrite the factorial function using a `while` loop.
 pub fn factorial(n: u32) -> u32 {
-    // The `todo!()` macro is a placeholder that the compiler
-    // interprets as "I'll get back to this later", thus
-    // suppressing type errors.
-    // It panics at runtime.
-    todo!()
+    let mut total = 1;
+    let mut other = n;
+    while other > 0 {
+        total *= other;
+        other -= 1;
+    }
+    return total;
 }
 
 #[cfg(test)]
