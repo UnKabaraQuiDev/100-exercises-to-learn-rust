4ffc64ac71a032d6317e57ebbd58afbc8597bffb
UnKabaraQuiDev <u.kbra.lu@gmail.com>
2026-06-10 17:01:42 +0200

    04_traits

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,7 +4,13 @@ pub struct WrappingU32 {
     value: u32,
 }
 
-fn example() {
-    let wrapping: WrappingU32 = 42.into();
-    let wrapping = WrappingU32::from(42);
+impl From<u32> for WrappingU32 {
+    fn from(val: u32) -> WrappingU32 {
+        WrappingU32 { value: val }
+    }
 }
+
+// fn example() {
+//     let _wrapping: WrappingU32 = 42.into();
+//     let _wrapping = WrappingU32::from(42);
+// }
